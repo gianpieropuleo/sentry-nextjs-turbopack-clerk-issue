19dd8b7a7a31e63aa9a9accc1937532583bbbe77
Gianpiero Puleo <gianpiero.puleo@gmail.com>
2026-03-02 22:37:00 +0700

    Minimal repo to reproduce issue

---
+++ app/[...rest]/page.tsx
@@ -0,0 +1,10 @@
+export default function CatchAll() {
+  return (
+    <main>
+      <h1>Catch-all route</h1>
+      <p>
+        This page is rendered for all routes that don&apos;t match other pages.
+      </p>
+    </main>
+  );
+}
